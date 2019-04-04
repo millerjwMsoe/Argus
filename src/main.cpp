737580c8ac5241bc883e60a086f385da41046ffe
Jonathan Enderle <enderlej@msoe.edu>
2019-04-04 22:12:54 +0000

    Write code to support two motors running simultaneously

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,23 @@ using namespace std;
 using namespace exploringRPi;
 
 int main(int argc, char* argv[]) {
-  Stepper* stepper = new ULN2003(6,16,20,21, "Stepper");
-  IRReceiver* receiver = new TSOP38238(12);
+  Stepper* r_stepper = new ULN2003(6, 16, 20, 21, "RightStepper");
+  Stepper* l_stepper = new ULN2003(22, 27, 17, 4, "LeftStepper");
+  IRReceiver* r_receiver = new TSOP38238(12);
+  IRReceiver* l_receiver = new TSOP38238(25);
 
-  AngleFinder* angleFinder = new AngleFinder(stepper, receiver, "Angle Finder");
-  angleFinder->setTaskPeriod(10);
-  angleFinder->start();
-  angleFinder->waitForShutdown();
+  r_stepper->startRotation(Stepper::CW);
+  l_stepper->startRotation(Stepper::CCW);
+  int i=0;
+  while(true) {
+    cout << "Check #: " << i++ << endl;
+    cout << "Left: " << l_receiver->lightDetected() << endl;
+    cout << "Right: " << r_receiver->lightDetected() << endl;
+    usleep(1000000);
+  }
+
+  AngleFinder* r_angleFinder = new AngleFinder(r_stepper, r_receiver, "RightAngleFinder");
+//  angleFinder->setTaskPeriod(10);
+//  angleFinder->start();
+//  angleFinder->waitForShutdown();
 }
