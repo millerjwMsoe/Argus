2be5c86b35afa48908d13e3b3c2c485a5f58f8c3
Jonathan Enderle <enderlej@msoe.edu>
2019-04-04 22:39:09 +0000

    Changes to seeking algorithm. It doesn't start correctly, and fails to reverse the search angle.

--- src/angleFinder.cpp
+++ src/angleFinder.cpp
@@ -12,35 +12,41 @@ AngleFinder::AngleFinder(Stepper* stepper, IRReceiver* receiver, string threadNa
     m_stepper = stepper;
     m_receiver = receiver;
     m_seeking = true;
+    m_searchCount = 1;
     m_start = 0;
-    m_target = 0;
+    m_target = m_start + m_searchCount*m_searchAngle;
     m_direction = Stepper::CW;
 }
 
 void AngleFinder::run() {
-    if(m_receiver->lightDetected()) {
+    if(m_receiver->lightDetected()) { // currently getting signal
         cout << "Bin at: " << m_stepper->getAngle() << endl;
         m_stepper->stopRotation();
         m_seeking = false;
-        m_searchCount = 1;
-    } else {
+    } else if(!m_seeking) { // just lost signal -> start looking
+        cout << "Lost signal." << endl;
         m_seeking = true;
+        m_searchCount = 1;
         double current = m_stepper->getAngle();
         m_target = current + m_searchCount*m_searchAngle;
         m_direction = Stepper::CW;
-    }
+        m_stepper->startRotation(m_direction);
+    } // else: already seeking signal
 
     if(m_seeking) {
+        cout << "Seeking..." << endl;
         if(m_direction == Stepper::CW) {
             if(m_stepper->getAngle() > m_target) {
                 m_searchCount++;
                 m_direction = opposite(m_direction);
                 m_target = m_start - m_searchCount*m_searchAngle;
+                m_stepper->startRotation(m_direction);
             }
         } else { // m_direction == Stepper::CCW
                 m_searchCount++;
                 m_direction = opposite(m_direction);
                 m_target = m_start + m_searchCount*m_searchAngle;
+                m_stepper->startRotation(m_direction);
         }
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -19,18 +19,12 @@ int main(int argc, char* argv[]) {
   IRReceiver* r_receiver = new TSOP38238(12);
   IRReceiver* l_receiver = new TSOP38238(25);
 
-  r_stepper->startRotation(Stepper::CW);
-  l_stepper->startRotation(Stepper::CCW);
-  int i=0;
-  while(true) {
-    cout << "Check #: " << i++ << endl;
-    cout << "Left: " << l_receiver->lightDetected() << endl;
-    cout << "Right: " << r_receiver->lightDetected() << endl;
-    usleep(1000000);
-  }
-
   AngleFinder* r_angleFinder = new AngleFinder(r_stepper, r_receiver, "RightAngleFinder");
-//  angleFinder->setTaskPeriod(10);
-//  angleFinder->start();
-//  angleFinder->waitForShutdown();
+  AngleFinder* l_angleFinder = new AngleFinder(l_stepper, l_receiver, "LeftAngleFinder");
+  r_angleFinder->setTaskPeriod(500);
+  l_angleFinder->setTaskPeriod(500);
+  r_angleFinder->start();
+  l_angleFinder->start();
+  r_angleFinder->waitForShutdown(); // blocks
+
 }
