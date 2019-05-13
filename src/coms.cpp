83e05517d866a21cc6e8c59f2c15dc0233fdba17
Joseph Miller <millerjw@msoe.edu>
2019-05-13 23:27:42 +0100

    Add JSON code to pi. Deserialize JSON message from Arduino

--- src/coms.cpp
+++ src/coms.cpp
@@ -7,12 +7,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
+#include <string>
+#include <sstream>
+#include "json.hpp"
 #include "coms.h"
 #include "rs232.h"
 
 #define BUF_SIZE 1024
 
 using namespace std;
+using json = nlohmann::json;
 
 Coms::Coms(string threadName) : PeriodicTask (threadName) {
     m_comport = RS232_GetPortnr("ttyACM0"); // TODO: determine this programatically?
@@ -34,6 +38,24 @@ void Coms::run() {
 	int n = RS232_PollComport(m_comport, str_recv, (int)BUF_SIZE);
 	if(n > 0){
 		str_recv[n] = 0;
-		printf("Received %i bytes: '%s'\n", n, (char *)str_recv);
+		//printf("Received %i bytes: '%s'\n", n, (char *)str_recv);
+	}
+        ostringstream oss;
+        for(int i=0; i<n; i++) {
+            oss << str_recv[i];
+        }
+        auto str = oss.str();
+        try {
+        	auto j = json::parse(str);
+		cout << "Distance 1 [mm]: " << j["DIST1"] << endl;
+		cout << "Distance 2 [mm]: " << j["DIST2"] << endl;
+		cout << "Left Front [RPM]: " << j["LF_RPM"] << endl;
+		cout << "Right Front [RPM]: " << j["RF_RPM"] << endl;
+		cout << "Left Rear [RPM]: " << j["LR_RPM"] << endl;
+		cout << "Right Rear [RPM]: " << j["RR_RPM"] << endl;
+		cout << "Bucket Volume [%]: " << j["VOLUME"] << endl;
+		cout << "Bucket Mass [kg]: " << j["MASS"] << endl;
+	} catch (nlohmann::detail::parse_error e) {
+		cout << "bad packet" << endl;
 	}
 }
